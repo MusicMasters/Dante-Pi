cdd54876ff985734782bf86010b702bb5661e700
aagellner <30661681+aagellner@users.noreply.github.com>
2022-02-02 18:49:11 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+sudo apt install -y git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64
+git clone --depth=1 https://github.com/raspberrypi/linux
+cd linux
+KERNEL=kernel8
+make -j $(( $nproc + $(nproc) / 2 )) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ../dante-bcm2711_defconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig
+make -j $(( $nproc + $(nproc) / 2 )) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
