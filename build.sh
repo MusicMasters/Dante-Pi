d59b9107b41774cc32ec2bb5e3cfbf433e9e0d8e
aagellner <30661681+aagellner@users.noreply.github.com>
2022-02-02 19:00:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 sudo apt install -y git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64
 git clone --depth=1 https://github.com/raspberrypi/linux
+cp dante-bcm2711_defconfig linux/
 cd linux
 KERNEL=kernel8
 make -j $(( $nproc + $(nproc) / 2 )) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ../dante-bcm2711_defconfig
